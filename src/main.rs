a433f473b8a991f1ef9fc5bb4e4e26f2024f904f
Suharjin <suharjin01@gmail.com>
2025-02-27 20:23:10 +0800

    Level

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+#[cfg(test)]
+mod tests {
+    use log::{debug, error, info, trace, warn};
+
+
+
+    // Level
+    #[test]
+    fn test_log() {
+        error!("This is an error");
+        warn!("This is a warning");
+        info!("This is an info");
+        debug!("This is a debaug");
+        trace!("This is a trace");
+    }
+}
